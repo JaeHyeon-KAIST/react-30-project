ec9d662150f9f4677ce60a993c3239aa01b6a591
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2023-01-18 23:50:44 +0900

    Quote

---
+++ 19.Quote/src/components/Box.jsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import { border, color, layout, display, flexbox, typography, space, position } from 'styled-system';
+
+const Box = styled.div`
+  ${layout}
+  ${color}
+  ${border}
+  ${display}
+  ${flexbox}
+  ${typography}
+  ${space}
+  ${position}
+`
+
+export default Box;
