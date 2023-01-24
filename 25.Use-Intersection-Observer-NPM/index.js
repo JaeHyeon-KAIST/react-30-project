8cec0989f756d92b34c64387ada536a7a134af94
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2023-01-24 23:38:40 +0900

    Use Intersection Observer : NPM Package

---
+++ 25.Use-Intersection-Observer-NPM/index.js
@@ -0,0 +1,18 @@
+import React, { useEffect, useState } from 'react'
+
+export default function useIntersectionObserver(ref) {
+  const [isIntersecting, setIsIntersecting] = useState(false)
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const IO = new IntersectionObserver(([entry]) => setIsIntersecting(entry.isIntersecting))
+
+    IO.observe(ref.current)
+
+    return () => {
+      IO.disconnect()
+    }
+  }, [ref])
+
+  return isIntersecting;
+}
