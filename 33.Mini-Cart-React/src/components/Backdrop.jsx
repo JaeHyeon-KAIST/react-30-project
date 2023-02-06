d795f0ae3d13e150ca04d34beba456f0df1d71af
JaeHyeon-KAIST <alvinlog@kaist.ac.kr>
2023-02-06 11:54:39 +0900

    Mini Cart React

---
+++ 33.Mini-Cart-React/src/components/Backdrop.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Backdrop({onClickHandler}) {
+  return (
+    <div
+      id="backdrop"
+      onClick={onClickHandler}
+      className="absolute inset-0 bg-gray-500 bg-opacity-75 transition-opacity"
+    />
+  )
+}
+
+export default Backdrop
